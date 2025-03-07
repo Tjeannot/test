c7996cb085501dc34f13395402fd1e6af073a80e
RICHER Danielle <danielle@air-de-richer.home>
2025-03-07 19:58:50 +0100

    Version propre spyder, manque influence du vent question bonus et explication 1ère question

--- question_1/question1.py
+++ question_1/question1.py
@@ -5,9 +5,24 @@ Created on Thu Mar  6 19:16:27 2025
 
 @author: danielle
 """
+#%% Imports des librairies
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+
+# %% Tests de verif
+
+# print ("speed : " , output_speed[8])
+# print ("altitude", output_altitude[8])
+# # print("wind", output_wind[4])
+# print("fuel_flow", output_fuel_flow[8])
+
+
+
+# %% Chargement des données depuis les fichiers pickle
 
 output_altitude = pd.read_pickle("signals_altitude.pkl")
 output_speed = pd.read_pickle("signals_vitesse.pkl")
@@ -16,72 +31,48 @@ output_fuel_flow = pd.read_pickle("signals_fuel_flow.pkl")
 
 
 
-# print ("speed : " , output_speed[8])
-# print ("altitude", output_altitude[8])
-# print("wind", output_wind[4])
-# print("fuel_flow", output_fuel_flow[8])
 
+# %% Fonctions pour l'étude de la consomation de carburant en fonction de la vitesse
 
-
-# Chargement des données depuis un fichier pickle
-output_altitude = pd.read_pickle("signals_altitude.pkl")
-output_speed = pd.read_pickle("signals_vitesse.pkl")
-output_fuel_flow = pd.read_pickle("signals_fuel_flow.pkl")
-
-# Nombre de vols
 Nb_col = output_altitude.shape[1]
 
 fuel_speed_list=[]
 
-def fuel_flow_model(vitesse_data, altitude_data, fuel_flow_data, altitude):
+def fuel_flow_model_1(vitesse_data, altitude_data, fuel_flow_data, altitude):
     #Initialisation
-    # fuel_speed_list=[]
     fuel_flow_list=[]
     vitesse_list=[]
+    
     for j in range(Nb_col):
+        
         #Initialisation des listes pour les valeurs de vitesse et altitudes à 8000 pieds
         valid_speeds=[]
         valid_flows=[]
-        found_altitude_data = False
+        
         for i in range (len(altitude_data[j])):
-            if altitude_data[j][i] == altitude:
-                found_altitude_data=True
-                if fuel_flow_data[j][i] >= 1:
-                    continue
+            
+            if altitude_data[j][i] == altitude and fuel_flow_data[j][i] >= 1 :
                 #Construction pour chaque vol, des vitesses et fuel flows à 8000 pieds
                 valid_speeds.append(vitesse_data[j][i])
                 valid_flows.append(fuel_flow_data[j][i])
                 
-                
-        if found_altitude_data:
-            
-            # Construction de la liste de données vitesse et fuel_flows à 8000 pieds
-            # fuel_speed_list.append(np.array([valid_speeds,valid_flows]))
-            fuel_flow_list.append(valid_flows)
-            vitesse_list.append(valid_speeds)
+        fuel_flow_list.append(valid_flows)
+        vitesse_list.append(valid_speeds)
     return vitesse_list, fuel_flow_list
     
-
 
 
 def IndexPlotFueLFlow_VS_Speed(fuel_flow, var, index):
     """
     Crée un graphique de dispersion du flux de carburant en fonction de la vitesse.
-
-    :param df: DataFrame contenant les données avec les colonnes 'speed' et 'fuel_flow'.
     """
-    fuel_flow = np.asarray(fuel_flow[index]) # Ensures data is in array form
+    fuel_flow = np.asarray(fuel_flow[index])
     var= np.asarray(var[index])
 
-    
     plt.subplot()
     plt.scatter(var, fuel_flow, color='b', alpha=0.7)
-    plt.title('Fuel Flow en Fonction de la Vitesse (à 8000 pieds)')
-    plt.xlabel('Vitesse (km/h)')
-    plt.ylabel('Fuel Flow (pound/s)')
     plt.grid(True)
     # plt.show()
-    
     
     
 def PlotFueLFlow_VS_Speed(fuel,speed):
@@ -95,19 +86,76 @@ def PlotFueLFlow_VS_Speed(fuel,speed):
     fuel_array = np.concatenate(fuel)
     ax.scatter(speed_array, fuel_array, color='b', alpha=0.7)
     
-    ax.set_title('Fuel Flow en Fonction de la Vitesse (à 8000 pieds)')
-    ax.set_xlabel('Vitesse (km/h)')
+    ax.grid(True)
+    
+    plt.show()
+
+# %%
+
+def fuel_flow_model_2(vitesse_data, altitude_data, fuel_flow_data, vitesse):
+    #Initialisation
+    # fuel_speed_list=[]
+    fuel_flow_list=[]
+    altitude_list=[]
+    for j in range(Nb_col):
+        #Initialisation des listes pour les valeurs de vitesse et altitudes à 8000 pieds
+        valid_altitudes=[]
+        valid_flows=[]
+
+        for i in range (len(altitude_data[j])):
+            if vitesse_data[j][i] == vitesse and fuel_flow_data[j][i] < 1 and 0 <= altitude_data[j][i] <= 15000:
+                #Construction pour chaque vol, des vitesses et fuel flows à 8000 pieds
+                valid_altitudes.append(altitude_data[j][i])
+                valid_flows.append(fuel_flow_data[j][i])
+
+        fuel_flow_list.append(valid_flows)
+        altitude_list.append(valid_altitudes)
+    return altitude_list, fuel_flow_list
+
+
+def IndexPlotFuelFlow_VS_altitude( fuel_flow, var, index):
+    """
+    Crée un graphique de dispersion du flux de carburant en fonction de 
+    l'altitude à une vitesse de 665km/h.
+    """  
+    fuel_flow = np.asarray(fuel_flow[index])
+    var= np.asarray(var[index])
+
+    
+    plt.subplot()
+    plt.scatter(var, fuel_flow, color='b', alpha=0.7)
+    plt.grid(True)
+
+def PlotFueLFlow_VS_Altitude(fuel, altitude):
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    
+    for i in range (len(fuel)):
+        IndexPlotFuelFlow_VS_altitude(fuel, altitude, i)
+
+
+    altitude_array = np.concatenate(altitude)
+    fuel_array = np.concatenate(fuel)
+    
+    # Plot all data on a single set of axes
+    ax.scatter(altitude_array, fuel_array, color='b', alpha=0.7)
+    ax.set_title("Fuel Flow en fonction de l'altitude (à 665 km/h)")
+    ax.set_xlabel('Valtitude(fts)')
     ax.set_ylabel('Fuel Flow (pound/s)')
     ax.grid(True)
     
     plt.show()
 
-    
-speed, fuel = fuel_flow_model(output_speed, output_altitude, output_fuel_flow, 8000.0)
 
-print(len(speed))
-# print(fuel)
-PlotFueLFlow_VS_Speed(fuel, speed)
+# %% Etude de la consomation de carburant en fonction de la vitesse pour une altitude de 8000 pieds
+
+speed, fuel_v = fuel_flow_model_1(output_speed, output_altitude, output_fuel_flow, 8000.0)
+PlotFueLFlow_VS_Speed(fuel_v, speed)
+
+# %% Etude de la consomation de carburant en fonction de l'altitude pour une vitesse de 665km/h
+
+altitude, fuel_a = fuel_flow_model_2(output_speed, output_altitude, output_fuel_flow, 665.0)
+PlotFueLFlow_VS_Altitude(fuel_a, altitude)
 
 
 
